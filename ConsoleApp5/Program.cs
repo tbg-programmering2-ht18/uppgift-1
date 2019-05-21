fe6a9673e7e1479967ab5a8e250742be0853ae35
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2019-05-21 11:10:15 +0200

    final

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,6 +10,36 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Ange ett decimal: ");
+            string inputFromUser = Console.ReadLine();
+            try
+            {
+                
+                string[] tiondel = inputFromUser.Split('.');
+                float FloatInput = int.Parse(tiondel[1]);
+                if(tiondel[0] != "0")
+                {
+                    Console.WriteLine("ser ut som du använder heltal, det skulle normalt crasha programmet, \n" + 
+                        "fast jag är en kol person så jag tar bara bort dom för dig automatiskt");
+
+                }
+
+                if(tiondel[1] == null)
+                {
+                    Console.WriteLine("Fel format");
+                }
+                else
+                {
+                    Console.WriteLine("0." + FloatInput);
+                }
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Wrong format");
+                Console.ReadKey();
+            }
         }
     }
 }
